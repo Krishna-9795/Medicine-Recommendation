6efcd949633d7a9d035018c964f473b448e60521
Krishna-9795 <127388066+Krishna-9795@users.noreply.github.com>
2023-03-10 17:32:48 +0530

    Update passwordd.py

--- passwordd.py
+++ passwordd.py
@@ -1,9 +1,12 @@
 import random
+
 lower_case='abcdefghijklmnopqrstuvwxyz'
 upper_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers="0123456789"
 symbols='!@#$%^&*?/'
+
 use_for=lower_case+upper_case+numbers+symbols
+#adsffd
 length_pass=8
 password="".join(random.sample(use_for,length_pass)) 
 print('your password generated=',password)
