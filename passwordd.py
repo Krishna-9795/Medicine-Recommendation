7f0e0e271365debe6d923a6952791fc9eef8282a
Krishna-9795 <127388066+Krishna-9795@users.noreply.github.com>
2023-03-10 17:31:54 +0530

    Add files via upload

---
+++ passwordd.py
@@ -0,0 +1,9 @@
+import random
+lower_case='abcdefghijklmnopqrstuvwxyz'
+upper_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers="0123456789"
+symbols='!@#$%^&*?/'
+use_for=lower_case+upper_case+numbers+symbols
+length_pass=8
+password="".join(random.sample(use_for,length_pass)) 
+print('your password generated=',password)
